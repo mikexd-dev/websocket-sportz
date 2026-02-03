f2417747b6fba9b4c3c8e89a639c450723de3c77
Mike Zhang <mikexd.dev@gmail.com>
2026-02-03 14:22:31 +0800

    validation zod

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
 import express from "express";
+import { matchRouter } from "./routes/matches.js";
 
 const app = express();
+app.use(express.json());
 
-app.get("/", (req, res) => {
-  res.send("Hello World");
-});
+app.use("/matches", matchRouter);
 
 app.listen(3000, () => {
   console.log("Server is running on port 3000");
